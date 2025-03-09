341a2680ea168a720b2849d0f58d08a01abc429f
240321 <2692231726@qq.com>
2025-03-09 23:07:27 +0800

    [any message]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -13,11 +13,15 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
 	if err != nil {
-		fmt.Fprint(os.Stderr, err)
+
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+
 		os.Exit(1)
+
 	}
-	fmt.Println(command[:len(command)-1] + " command not found")
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }

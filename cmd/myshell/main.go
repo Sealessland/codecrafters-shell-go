379287fcfaba8009138bc421cfe89d7515c7f50c
240321 <2692231726@qq.com>
2025-03-09 23:32:49 +0800

    [any message]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,10 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
-
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
 
 func main() {
 	for {
@@ -19,11 +17,18 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		switch command {
-		case "exit 0\n":
+
+		// Trim the newline character
+		command = strings.TrimSpace(command)
+
+		switch {
+		case command == "exit 0":
 			os.Exit(0)
+		case strings.HasPrefix(command, "echo "):
+			// Print the rest of the command after "echo "
+			fmt.Println(command[5:])
 		default:
-			fmt.Println(command[:len(command)-1] + ": command not found")
+			fmt.Println(command + ": command not found")
 		}
 	}
 }
